f494f8eec6b91696b41563d49449d92120bb9263
Sebastian <sebahomsi@gmail.com>
2020-03-09 12:54:40 -0300

    commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,20 @@
+const opciones = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opciones)
+    .command('crear', 'crea archivos', opciones)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
